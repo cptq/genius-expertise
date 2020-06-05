580fdc7ec6b291172aa0707adab10d867b4b9a3b
Derek Lim <lim.derek@yahoo.com>
2020-06-04 20:31:15 -0400

    rename constants

--- constants.py
+++ constants.py
@@ -1,25 +1,26 @@
 from datetime import datetime
 
-datapath = "spiders/data"
-figpath = "figures"
+DATAPATH = "data"
+FIGPATH = "figures"
 
+# user stats
 STATS = ("Annotations", "Suggestions", "Transcriptions", "Questions",
              "Answers", "Pyongs")
-# user stats
+
+# user stats including iq
 ALL_STATS = ("iq", "Annotations", "Suggestions", "Transcriptions", "Questions",
              "Answers", "Pyongs")
+
 # user stats which we plot for publication
 PUB_STATS = ("iq", "Annotations")
-
              
+# roles that users may assume
 ROLES = ("Contributor", "Editor", "Mediator", "Moderator", "Staff")
+# roles including verified artists
 A_ROLES = ("Verified Artist", "Contributor", "Editor", "Mediator", "Moderator", "Staff")
-
-# roles which we plot for publication
-PUB_ROLES = ('Editor', 'Mediator', 'Moderator', 'Staff')
 
 GENIUS_LAUNCH_TIME = datetime(2009, 10, 20).timestamp()
 
-# colors
+# colors we use in plots
 PRED = '#B33274'
 CYCLE_COLORS = ['#442AB8', '#B80717', '#149934']
