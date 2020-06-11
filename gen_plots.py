e9e5cbb056644458d32b4c4f602a543373cbcd8c
Derek Lim <lim.derek@yahoo.com>
2020-06-11 11:33:29 -0400

    finished all figures

--- gen_plots.py
+++ gen_plots.py
@@ -272,6 +272,48 @@ def figure5b():
         print(f'Saved to {namestr}')
         count += 1
 
+def figure6():
+    def ann_prop_time_rank_to_lyric_score(min_annots=10):
+        song_to_lyrics, _ = make_song_to_lyrics(annotation_info)
+        vectorizer = lyric_vectorizer(song_to_lyrics)
+        song_to_annot_idx = collections.defaultdict(list)
+        x, y = [], []
+        for i, a in enumerate(annotation_info):
+            if a['type'] == 'reviewed':
+                song_name = a['song']
+                song_to_annot_idx[song_name].append(i)
+        for s in song_to_annot_idx:
+            song_to_annot_idx[s].sort(key=lambda i:
+                                to_dt_a(annotation_info[i]['time']).timestamp())
+            idx_lst = song_to_annot_idx[s]
+            if len(idx_lst) >= min_annots:
+                content_lst = [BeautifulSoup(
+                    annotation_info[i]['edits_lst'][-1]['content']).get_text()
+                              for i in idx_lst ]
+                score_matrix = vectorizer.transform(content_lst)
+                for tr, i in enumerate(idx_lst):
+                    prop_time_rank = tr/(len(idx_lst)-1)
+                    if score_matrix[tr, :].nnz:
+                        _, _, v = scipy.sparse.find(score_matrix[tr,:])
+                        score = (np.percentile(v, 60) + np.percentile(v, 75) + np.percentile(v, 90))/3
+                    else:
+                        score = 0
+                    if score:
+                        x.append(prop_time_rank)
+                        y.append(score-1) # -1 to align with paper defn
+        return x, y
+    x, y = ann_prop_time_rank_to_lyric_score(min_annots=10)
+    one_col()
+    plt.figure(figsize=(2, 1.2))
+    xb, yb = make_cont_bins(x, y, num_bins=15)
+    yb = [np.mean(b) for b in yb]
+    plt.plot(xb, yb, '-o', color=PRED)
+    plt.xlabel('q')
+    plt.ylabel('Originality')
+    plt.yticks([4.9, 4.95, 5, 5.05, 5.1])
+    plt.tight_layout(pad=0)
+    plt.savefig('figures/figure6.pdf')
+
 def figure8a():
     def split_editnum_vs_stat(stat, most_edits=10):
         """edit time rank vs stat
@@ -349,6 +391,175 @@ def figure8b():
         plt.savefig(namestr)
         print(f'Saved to {namestr}')
 
+def figure9b():
+    def date_user_edit_evolution(min_user_edits=10, valtype=0, max_days=750):
+        ylab = {
+        0: 'First edits',
+        }
+        user_to_edits = collections.defaultdict(list)
+        user_to_iq = {u['url_name']:u['iq'] for u in user_info_gen()}
+        utypes = ['high iq', 'mid iq', 'low iq', 'all']
+        for i, a in enumerate(annotation_info):
+            if a['type'] == 'reviewed':
+                song_name = a['song']
+                u = a['edits_lst'][-1]['name'].split('/')[-1]
+                for time_rank, e in enumerate(list(reversed(a['edits_lst']))[1:]):
+                    true_time_rank = time_rank+2
+                    user_to_edits[u].append((i, true_time_rank))
+                    
+        # first edit datetimes
+        user_first_dt = {}
+        for u in user_to_edits:
+            user_to_edits[u].sort(key = lambda t:
+                                 to_dt_a(
+                                     annotation_info[t[0]]['edits_lst'][::-1][t[1]-1]['time']).timestamp()
+                                 )
+            first_idx, true_time_rank = user_to_edits[u][0]
+            user_first_dt[u] = to_dt_a(
+                list(reversed(annotation_info[first_idx]['edits_lst']))[true_time_rank-1]['time']
+                    )
+        
+        xs, ys = collections.defaultdict(list), collections.defaultdict(list)
+        for u in user_to_edits:
+            num_user_edits = len(user_to_edits[u])
+            if num_user_edits >= min_user_edits and u in user_to_iq:
+                iq = user_to_iq[u]
+                if iq > 1e5:
+                    utype = utypes[0]
+                elif iq > 1e4:
+                    utype = utypes[1]
+                else:
+                    utype = utypes[2]
+                for life_time_rank, (idx, true_time_rank) in enumerate(user_to_edits[u]):
+                    time_rank = true_time_rank-1
+                    a = annotation_info[idx]
+                    o_edits_lst = a['edits_lst'][::-1]
+                    elapsed = to_dt_a(o_edits_lst[time_rank]['time']) - user_first_dt[u]
+                    if elapsed.days > max_days:
+                        break
+                    
+                    if valtype==0:
+                        val = int(time_rank == 1)
+                    
+                    xs[utype].append(elapsed.days)
+                    ys[utype].append(val)
+                    xs['all'].append(elapsed.days)
+                    ys['all'].append(val)
+        for utype in utypes:
+            if utype == 'low iq':
+                continue
+            x, y = xs[utype], ys[utype]
+            x, y = make_cont_bins(x, y, num_bins=21)
+            yout = [[] for _ in range(len(y))]
+            for i in range(len(y)):
+                for j in range(i+1):
+                    yout[i].extend(y[j])
+                yout[i] = np.mean(yout[i])
+            # omits very first edit
+            plt.plot(x[1:], yout[1:], '-o')
+        plt.xlabel('User Age')
+        plt.ylabel(ylab[valtype])
+        plt.xticks([0,250,500,750])
+    valtype = 0
+    two_col()
+    fig, ax = plt.subplots(1)
+    ax.set_prop_cycle('color', CYCLE_COLORS)
+    date_user_edit_evolution(min_user_edits=10, valtype=valtype)
+    plt.tight_layout(pad=0)
+    namestr = f'{FIGPATH}/figure9-1-{valtype}.pdf'
+    plt.savefig(namestr)
+    print(f'Saved to {namestr}')
+
+def figure9a():
+    def date_split_user_ann_evolution(ax, min_user_anns=10, valtype=0, max_days = 750):
+        ylab = {
+        1: 'First ann.',
+        4: 'Quality Tags',
+        5: 'Length',
+        7: 'Lyric Originality',
+        8: 'Song Originality'
+        }
+        user_to_ann_idx = make_user_to_ann_idx(annotation_info)
+        song_to_ann_idx = make_song_to_ann_idx(annotation_info)
+        ann_idx_to_prop_time = {}
+        user_to_iq = {u['url_name']:u['iq'] for u in user_info_gen()}
+        xs, ys = collections.defaultdict(list), collections.defaultdict(list)
+        for s in song_to_ann_idx:
+            num_anns = len(song_to_ann_idx[s])
+            for time_rank, idx in enumerate(song_to_ann_idx[s]):
+                ann_idx_to_prop_time[idx] = time_rank/(num_anns-1) if num_anns > 1 else 0
+        # first annotation datetimes
+        user_first_dt = {}
+        for u in user_to_ann_idx:
+            first_idx = user_to_ann_idx[u][0]
+            user_first_dt[u] = to_dt_a(annotation_info[first_idx]['time'])
+            
+        utypes = ['high iq', 'mid iq', 'low iq', 'all']
+        ann_high_tile = 10**5
+        ann_midup_tile = 5*10**4
+        ann_midlow_tile = 10**4
+
+        for u in user_to_ann_idx:
+            num_user_anns = len(user_to_ann_idx[u])
+            if num_user_anns >= min_user_anns and u in user_to_iq:
+                iq = user_to_iq[u]
+                if iq > ann_high_tile:
+                    utype = utypes[0]
+                elif ann_midup_tile > iq >= ann_midlow_tile:
+                    utype = utypes[1]
+                else:
+                    utype = utypes[2]
+                for idx in user_to_ann_idx[u]:
+                    a = annotation_info[idx]
+                    elapsed = to_dt_a(a['time']) - user_first_dt[u]
+                    if elapsed.days > max_days:
+                        break
+                    content = a['edits_lst'][-1]['content']
+                    if valtype==1:
+                        val = int(ann_idx_to_prop_time[idx] == 0)
+                    elif valtype==4:
+                        val = num_quality_tags(content, quality_tags)
+                    elif valtype==5:
+                        val = len(BeautifulSoup(content).get_text())
+                    elif valtype==7:
+                        val = ann_idx_to_score[idx]
+                    elif valtype==8:
+                        val = song_to_score.get(a['song'], None)
+                    if val is not None:
+                        xs[utype].append(elapsed.days)
+                        ys[utype].append(val)
+                        xs['all'].append(elapsed.days)
+                        ys['all'].append(val)
+        for utype in utypes:
+            if utype == 'low iq':
+                continue
+            x, y = xs[utype], ys[utype]
+            x, y = make_cont_bins(x, y, num_bins=15)
+            # cumulative average
+            yout = [[] for _ in range(len(y))]
+            for i in range(len(y)):
+                for j in range(i+1):
+                    yout[i].extend(y[j])
+                yout[i] = np.mean(yout[i])
+            # omits very first annotation
+            ax.plot(x[1:], yout[1:], '-o')
+        plt.xlabel('User Age')
+        plt.ylabel(ylab[valtype])
+        plt.xticks([0,250,500,750])
+
+    song_to_lyrics, song_to_ann_lyrics = make_song_to_lyrics(annotation_info)
+    song_to_score = compute_song_to_score(song_to_lyrics)
+    ann_idx_to_score = compute_ann_idx_to_score(song_to_lyrics, annotation_info)
+    two_col()
+    for valtype in (1,4,5,7,8):
+        fig, ax = plt.subplots(1)
+        ax.set_prop_cycle('color', CYCLE_COLORS)
+        date_split_user_ann_evolution(ax, min_user_anns=10, valtype=valtype)
+        plt.tight_layout(pad=0)
+        namestr = f'{FIGPATH}/figure9-0-{valtype}.pdf'
+        plt.savefig(namestr)
+        print(f'Saved to {namestr}')
+
 def figure10():
     def rank_vs_stat(ax, stat):
         G = load_graph(positive_iq=True)
@@ -399,7 +610,7 @@ if __name__ == '__main__':
     parser.add_argument('--figure', type=int, default=-1)
     args = parser.parse_args()
 
-    valid_figures = (2,3,4,5,8,10)
+    valid_figures = (2,3,4,5,6,8,9,10)
     print(f'Valid figures are: {valid_figures}')
     if args.figure in valid_figures:
         print('~~~Loading data~~~')
@@ -414,9 +625,14 @@ if __name__ == '__main__':
     elif args.figure == 5:
         figure5a()
         figure5b()
+    elif args.figure == 6:
+        figure6()
     elif args.figure == 8:
         figure8a()
         figure8b()
+    elif args.figure == 9:
+        figure9a()
+        figure9b()
     elif args.figure == 10:
         figure10()
     else:
--- process_lyrics.py
+++ process_lyrics.py
@@ -44,11 +44,17 @@ def tokenize_lyrics(lyrics):
     tokens = nltk.word_tokenize(lyrics)
     return tokens
 
+def lyric_vectorizer(song_to_lyrics):
+    vectorizer = TfidfVectorizer(tokenizer=tokenize_lyrics, binary=True, norm=None, smooth_idf=False)
+    vectorizer.fit(song_to_lyrics.values())
+    return vectorizer
+    
 def make_tfidf_matrix(song_to_lyrics):
     """Builds tfidf matrix on lyrics"""
-    vectorizer = TfidfVectorizer(tokenizer=tokenize_lyrics, binary=True,
-                                     norm=None, smooth_idf=False)
-    tfidf_matrix = vectorizer.fit_transform(song_to_lyrics.values())
+    #vectorizer = lyric_vectorizer()
+    #tfidf_matrix = vectorizer.fit_transform(song_to_lyrics.values())
+    vectorizer = lyric_vectorizer(song_to_lyrics)
+    tfidf_matrix = vectorizer.transform(song_to_lyrics.values())
     tfidf_matrix.data -= 1 # subtract 1 to agree with paper definition
     return tfidf_matrix
 
@@ -83,3 +89,33 @@ def covered_stat(song, song_to_lyrics, song_to_ann_lyrics):
         lyrics = lyrics.replace(t, '')
     covered_val = 1-len(lyrics)/prev_len
     return covered_val
+
+def compute_ann_idx_to_score(song_to_lyrics, annotation_info):
+    """ computes a dictionary from annotation index to originality
+    """
+    print('Computing annotation scores, takes a few minutes')
+    vectorizer = lyric_vectorizer(song_to_lyrics)
+    ann_idx_to_score = {}
+    song_to_annot_idx = collections.defaultdict(list)
+    for i, a in enumerate(annotation_info):
+        if a['type'] == 'reviewed':
+            song_name = a['song']
+            song_to_annot_idx[song_name].append(i)
+    for s in song_to_annot_idx:
+        song_to_annot_idx[s].sort(key=lambda i:
+                            to_dt_a(annotation_info[i]['time']).timestamp())
+        idx_lst = song_to_annot_idx[s]
+        content_lst = [BeautifulSoup(
+            annotation_info[i]['edits_lst'][-1]['content']).get_text()
+                      for i in idx_lst ]
+        score_matrix = vectorizer.transform(content_lst)
+                                            
+        score_matrix.data -= 1
+        for tr, i in enumerate(idx_lst):
+            if score_matrix[tr, :].nnz:
+                _, _, v = scipy.sparse.find(score_matrix[tr,:])
+                score = (np.percentile(v, 60) + np.percentile(v, 75) + np.percentile(v, 90))/3
+            else:
+                score = 0
+            ann_idx_to_score[i] = score
+    return ann_idx_to_score
